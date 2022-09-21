1264655c6677fc3c71e0d0b3653f46c6bab16bea
bignick1958 <bignick58@gmail.com>
2022-09-21 22:22:54 +0300

    Создание главного окна программы

---
+++ ChartExamples.py
@@ -0,0 +1,17 @@
+# Примеры построения графиков
+# автор: Bignick, bignick58@gmail.com
+
+import tkinter as tk
+
+
+# создание гоавного окна программы
+window = tk.Tk()
+window.geometry(f"300x300")
+window['bg'] = '#8fffff'
+window.title('My Python calculator')
+
+
+
+
+# щапусск цикла
+window.mainloop()
