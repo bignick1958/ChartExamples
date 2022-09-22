34527f16a7048110973f737ae6585e3e8d0535a9
bignick1958 <bignick58@gmail.com>
2022-09-22 07:57:59 +0300

    Добавление метки заголовка

--- ChartExamples.py
+++ ChartExamples.py
@@ -13,8 +13,13 @@ window.geometry("450x450")
 window['bg'] = '#8fffff'
 window.title('Примеры построения графиков')
 
-# добавление кнрпки закрытия
-btn_close = tk.Button(window, text="Закрыть", font=('Bahnschrift', 12, 'bold'))
+# добавление метки заголовка
+lbl_title = tk.Label(text='Примеры построения графиков', font=('Bahnschrift', 16, 'bold'  ), fg = '#0000cc', bg= '#8fffff')
+lbl_title.place(x=55, y=30)
+
+
+# добавление кнопки закрытия
+btn_close = tk.Button(window, text="Закрыть", font=('Bahnschrift', 12, 'bold'), command = do_close)
 btn_close.place(x=330, y=400, width = 90, height = 30)
 
 
